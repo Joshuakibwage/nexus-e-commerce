64a61e50535ce549eb3513e2cafd4effe9f53f1a
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-09-28 14:43:20 +0300

    feat: modified types component

--- types/index.ts
+++ types/index.ts
@@ -1,15 +1,21 @@
 export type Category = {
-    id: string;
-    name: string;
-    description: string;
+  id: string;
+  name: string;
+  description: string;
 };
 
 export type Product = {
-    id: string;
-    name: string;
-    description: string;
-    priceAmount: number;
-    currency: string;
-    images: { id: string }[];
-    category: Category;
+  id: string;
+  name: string;
+  description: string;
+  priceAmount: number;
+  currency: string;
+  images: { id: string }[];
+  category: Category;
+};
+
+export type FilterState = {
+  selectedCategory: string | null;
+  sortOrder: 'asc' | 'desc' | null;
+  searchQuery: string;
 };
