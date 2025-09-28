1d286c929a0b6ae594dc1f67eeefac50ff016f77
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-09-28 23:36:33 +0300

    docs: ready for deployment

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,6 +5,9 @@ import { ApolloProvider } from '@apollo/client/react';
 import { Provider } from 'react-redux';
 import apolloClient from '@/lib/apolloClient';
 import { store } from '@/store/store';
+import { ShoppingCart } from "lucide-react";
+import Link from "next/link";
+
 
 interface LayoutProps {
   children: ReactNode;
@@ -14,12 +17,25 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <ApolloProvider client={apolloClient}>
       <Provider store={store}>
-        <div className="min-h-screen bg-gray-50">
+        <div className="min-h-screen bg-gray-50 w-full">
             <header className="bg-white shadow-sm">
-                <div className="max-w-7xl mx-auto px-4 py-4">
-                    <h1 className="text-2xl font-bold text-gray-900">
-                        E-Commerce Catalog
-                    </h1>
+                <div className="max-w-[90%] mx-auto px-4 py-4 flex justify-between items-center">
+                    <Link href="/">
+                      <h1 className="text-2xl font-bold text-gray-900">
+                          E-Commerce Catalog
+                      </h1>
+                    </Link>
+
+
+                    <Link href="/cart" className="relative">
+                      <ShoppingCart 
+                        className="w-6 h-6 text-gray-600" 
+                      />
+                      <span className="absolute -top-3 -right-1 bg-blue-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-xs font-medium">
+                        
+                      </span>
+                    </Link>
+
                 </div>
             </header>
             <main className="max-w-7xl mx-auto px-4 py-6">
