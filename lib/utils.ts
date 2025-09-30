042350bec7a23c63b13aa641ab22eef1cf256f91
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-09-30 19:36:54 +0300

    feat: update utils component

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
-export function getImageUrl(imageId: string): string {
+export const getImageUrl = (imageId: string): string => {
   return `https://project-nexus-backend-q5ai.onrender.com/images/${imageId}`;
-}
+};
 
-export function formatPrice(amount: number, currency: string): string {
+export const formatPrice = (amount: number, currency: string): string => {
   return `${currency} ${amount.toFixed(2)}`;
-}
+};
